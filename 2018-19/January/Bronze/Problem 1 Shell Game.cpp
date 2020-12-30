260f9264cc0e3148d4150ede5e10ed8e71c422c7
AI-HBB <75945892+AI-HBB@users.noreply.github.com>
2020-12-30 10:38:34 -0500

    Update Problem 1 Shell Game.cpp

--- 2018-19/January/Bronze/Problem 1 Shell Game.cpp
+++ 2018-19/January/Bronze/Problem 1 Shell Game.cpp
@@ -6,14 +6,14 @@ using namespace std;
 int main() {
 freopen("shell.in", "r", stdin);
 freopen("shell.out", "w", stdout);
-int N,a[101],b[101],g[101],intial_shell=1,current_shell,best=0,ans=0;
+int N,a[101],b[101],g[101],initial_shell=1,current_shell,best=0,ans=0;
 cin >> N;
 for(int i=0; i<N; i++){
 cin >> a[i] >> b[i] >> g[i];
 }
-while (intial_shell!=4){
+while (initial_shell!=4){
 ans=0;
-current_shell=intial_shell;
+current_shell=initial_shell;
 for(int i=0; i<N; i++){
 if(a[i]==current_shell||b[i]==current_shell){
  if(a[i]==current_shell){
@@ -30,7 +30,7 @@ if(current_shell==g[i]){
 if(ans>best){
   best=ans;
 }
-intial_shell++;
+initial_shell++;
 }
 cout << best;
 }
